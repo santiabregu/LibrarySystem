7ed7a7638a2764a76e46d1fa74610deb9ed25ed8
jeton <jetonstojku@hotmail.com>
2023-08-09 19:32:11 +0200

    functions, onchange, compute, depends, odoo framework

--- commprog/models/invoice.py
+++ commprog/models/invoice.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import UserError
 
 
 class CommprogInvoice(models.Model):
@@ -16,9 +17,27 @@ class CommprogInvoice(models.Model):
                              selection=[('draft', 'Draft'), ('done', 'Done'), ('paid', 'Paid')])
     invoice_line_ids = fields.One2many(comodel_name='commprog.invoice.line', inverse_name='invoice_id',
                                        string='Invoice line')
+    total = fields.Float(string='Total', store=True, compute="_calc_total_invoice")
+
+    @api.depends('invoice_line_ids')
+    def _calc_total_invoice(self):
+        for invoice in self:
+            # s = 0
+            # for invoice_line in invoice.invoice_line_ids:
+            #     s += invoice_line.total
+            # invoice.total = s
+            invoice.total = sum(invoice.invoice_line_ids.mapped('total'))
 
     def confirm(self):
         self.state = 'done'
+        if self.invoice_type == 'in':
+            for invoice_line in self.invoice_line_ids:
+                invoice_line.product_id.quantity += invoice_line.quantity
+        else:
+            for invoice_line in self.invoice_line_ids:
+                if invoice_line.product_id.quantity < invoice_line.quantity:
+                    raise UserError("Quantity to low!")
+                invoice_line.product_id.quantity -= invoice_line.quantity
 
     def pay(self):
         self.state = 'paid'
@@ -35,3 +54,16 @@ class CommprogInvoiceLine(models.Model):
     price = fields.Float(string="Price", default=0, required=True)
     invoice_id = fields.Many2one(comodel_name='commprog.invoice', string='Invoice', required=True)
     product_id = fields.Many2one(comodel_name='commprog.product', string='Product', required=True)
+    total = fields.Float(string='Total', compute="_calc_total")
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        if self.invoice_id.invoice_type == 'in':
+            self.price = self.product_id.purchase_price
+        else:
+            self.price = self.product_id.sale_price
+
+    @api.depends('quantity', 'price')
+    def _calc_total(self):
+        for invoice_line in self:
+            invoice_line.total = invoice_line.quantity * invoice_line.price
